b1a5f469327e44266b00c772dc23afd6b3dbfacf
Niharika Saini <Niharika Saini@LAPTOP-612DADD7>
2022-04-02 14:23:28 +0530

    First Commit

---
+++ LearningJavaInDepth/src/Unit01/P1_Task01_FirstJavaProgram.java
@@ -0,0 +1,13 @@
+package Unit01;
+
+public class P1_Task01_FirstJavaProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!");
+		
+		System.out.println("This Is My First Java Program");
+		
+	}
+
+}
